99f5ec346d227d953df380900c2200d6d2262a63
Myriam <manki@student.42.fr>
2020-03-11 21:29:52 +0100

    step one done: table contact and model contact.rb created

---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < ActiveRecord::Base
+
+  validates :name, presence: {
+    message: "Name must be filled"
+  }
+
+  validates :phone, uniqueness: {
+    message: "A contact already exist with this phone number"
+  }
+
+  validates :email, uniqueness: {
+    message: "A contact already exist with this mail address"
+  }
+end
---
+++ db/migrate/20200311191326_table_contacts.rb
@@ -0,0 +1,10 @@
+class TableContacts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.text :interest
+    end
+  end
+end

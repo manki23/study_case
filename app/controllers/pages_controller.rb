e8809ab56c7d44e4cce99c8108afdab8680b157d
Myriam <manki@student.42.fr>
2020-03-12 23:30:07 +0100

    for step 6: :autocomplete => true added to select (I don't think that was what was asked) | step 7 done | a page admin added to see all user (contacts), update them, delete them and also see the full interests list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,19 +15,57 @@ class PagesController < ApplicationController
 
   def create
     @interests = Interest.all
-    if not @contact
+    if  !(@contact)
       @contact = Contact.create(name: params[:name], phone: params[:phone], email: params[:email])
       if params[:newsletter_subscriber]
         NewsletterSubscriber.create(subscriber_id: @contact.id)
       end
-        params[:multi].each do |one|
-          ContactInterest.create(contact_id: @contact.id, interest_id: one)
-        end
+      params[:multi].each do |one|
+        ContactInterest.create(contact_id: @contact.id, interest_id: one)
+      end
     end
     if @contact.errors.first
       render :new
     else
       render :home
     end
+  end
+  def admin
+    @contacts = Contact.all
+    @interests = Interest.all
+    @contact_interests = ContactInterest.all
+    @subs = NewsletterSubscriber.all
+  end
+  def update
+    @interests = Interest.all
+    @contact = Contact.find params[:id]
+    if @contact
+      if params[:name]
+        @contact.update name: params[:name]
+      end
+      if params[:phone]
+        @contact.update phone: params[:phone]
+      end
+      if params[:email]
+        @contact.update email: params[:email]
+      end
+      if params[:newsletter_subscriber] && !(NewsletterSubscriber.exists?(subscriber_id: @contact.id))
+        NewsletterSubscriber.update subscriber_id: @contact.id
+      elsif !params[:newsletter_subscriber] && (NewsletterSubscriber.exists?(subscriber_id: @contact.id))
+        @to_del = NewsletterSubscriber.find_by(subscriber_id: @contact.id)
+        @to_del.destroy if @to_del
+      end
+      params[:multi].each do |one|
+        if !(ContactInterest.exists?(contact_id: @contact.id, interest_id: one))
+          ContactInterest.create(contact_id: @contact.id, interest_id: one)
+        end
+      end
+    end
+    redirect_to "/admin"
+  end
+  def delete
+    @user = Contact.find(params[:user_id])
+    @user.destroy
+    redirect_to "/admin"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'index' => 'pages#index'
   get 'new' => 'pages#new'
   post 'new' => 'pages#create'
+  get 'admin' => 'pages#admin'
+  delete 'admin' => 'pages#delete'
+  patch 'admin' => 'pages#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

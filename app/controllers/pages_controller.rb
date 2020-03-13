a360a425843a5b9224c33cf870cc16f6a877936e
Myriam <manki@student.42.fr>
2020-03-13 11:41:25 +0100

    Optionnals part done, a bit of css added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -38,26 +38,42 @@ class PagesController < ApplicationController
   end
   def update
     @interests = Interest.all
-    @contact = Contact.find params[:id]
-    if @contact
+    @contact_interests = ContactInterest.all
+    if Contact.exists? params[:id]
+      @contact = Contact.find params[:id]
+      @save_name = @contact.name
+      @save_phone = @contact.phone
+      @save_email = @contact.email
       if params[:name]
-        @contact.update name: params[:name]
+        @save_name = params[:name]
+        @contact.update name: params[:name], phone: @save_phone, email: @save_email
       end
       if params[:phone]
-        @contact.update phone: params[:phone]
+        @save_phone = params[:phone]
+        @contact.update name: @save_name, phone: params[:phone], email: @save_email
       end
       if params[:email]
-        @contact.update email: params[:email]
+        @save_email = params[:email]
+        @contact.update name: @save_name, phone: @save_phone, email: params[:email]
       end
-      if params[:newsletter_subscriber] && !(NewsletterSubscriber.exists?(subscriber_id: @contact.id))
-        NewsletterSubscriber.update subscriber_id: @contact.id
-      elsif !params[:newsletter_subscriber] && (NewsletterSubscriber.exists?(subscriber_id: @contact.id))
-        @to_del = NewsletterSubscriber.find_by(subscriber_id: @contact.id)
-        @to_del.destroy if @to_del
+      if params[:newsletter_subscriber] && !(NewsletterSubscriber.exists?(subscriber_id: params[:id]))
+        NewsletterSubscriber.create subscriber_id: params[:id]
+      elsif !params[:newsletter_subscriber] && (NewsletterSubscriber.exists?(subscriber_id: params[:id]))
+        @to_del = NewsletterSubscriber.find_by(subscriber_id: params[:id])
+        @to_del.destroy
       end
-      params[:multi].each do |one|
-        if !(ContactInterest.exists?(contact_id: @contact.id, interest_id: one))
-          ContactInterest.create(contact_id: @contact.id, interest_id: one)
+      if params[:delete_old_interests]
+        @contact_interests.each do |one|
+          if one.contact_id == @contact.id
+            one.destroy
+          end
+        end
+      end
+      if params[:multi]
+        params[:multi].each do |one|
+          if !(ContactInterest.exists?(contact_id: @contact.id, interest_id: one))
+            ContactInterest.create(contact_id: @contact.id, interest_id: one)
+          end
         end
       end
     end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+  def display_interests contact_interests, interests, contact
+    count = 0
+    res = ""
+    contact_interests.each do |c_i|
+      if c_i.contact_id == contact.id && interests.exists?(c_i.interest_id)
+        res += ",  " if count != 0
+        res += "[" + @interests.find(c_i.interest_id).name + "]"
+        count += 1
+      end
+    end
+    res
+  end
 end
